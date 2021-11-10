9e74438c5b9e36abc9268baf6d9e34f22a504057
Carlos Jaramillo <carlos.jaramillo@newshore.es>
2021-11-10 14:27:44 -0500

    14. Servicios

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -5,20 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
 import { AgregarComponent } from './agregar/agregar.component';
-
+import { DbzService } from './services/dbz.service';
 
 @NgModule({
   declarations: [
     MainPageComponent,
     PersonajesComponent,
-    AgregarComponent
-  ],
+    AgregarComponent],
   exports: [
     MainPageComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule],
+  providers: [
+    DbzService
   ],
 })
 export class DbzModule {}
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
+  constructor(private dbzservice: DbzService) {}
+
   personajes: Personaje[] = [
     {
       nombre: 'Goku',
